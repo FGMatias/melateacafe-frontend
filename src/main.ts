b3ac879704b7e4efc467df1d9bffffa8a001e2d5
Matias Fong <matesgon828@gmail.com>
2025-12-05 23:31:03 -0500

    chore(project): update dependencies, configuration, and global styles - Updated package.json and package-lock.json after dependency adjustments - Modified angular.json configuration - Updated main.ts bootstrap logic - Refined global styles in styles.scss

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,29 @@
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
+import Aura from '@primeng/themes/aura';
+import { providePrimeNG } from 'primeng/config';
 import { App } from './app/app';
+import { routes } from './app/app.routes';
+import { authInterceptor } from './app/core/interceptors/auth.interceptor';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+          cssLayer: false
+        }
+      }
+    })
+  ]
+}).catch((err) => console.error(err));
